2ecbcd01ea23d6f40c759d00bcc6c2b1219c716a
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2023-03-26 11:57:35 +0530

    aside created

---
+++ components/aside.js
@@ -0,0 +1,28 @@
+import { BiPlus, BiComment, BiTrashAlt } from "react-icons/bi";
+
+export default () => {
+    return (
+        <aside className="fixed left-0 w-80 h-screen bg-gray-900">
+            <div className="text-gray-50 flex flex-col items-center py-3 gap-5">
+
+                <button className="border rounded-md border-gray-600 w-4/5 hover:bg-indigo-600">
+                    <span className="block py-3"><BiPlus className="inline" size={20} /> New Chat</span>
+                </button>
+
+                <div className="chat_list w-full flex flex-col gap-4 px-3">
+                    <div className="w-full border-0 rounded-md bg-gray-800 py-1 px-3 flex justify-center items-center">
+                        <button className="text-left truncate w-full active:bg-violet-700">
+                            <span className="block py-3 text-gray-50">
+                                <BiComment className="inline mx-2" size={20}></BiComment>
+                                Room 1
+                            </span>
+                        </button>
+                        <button className=" bg-gradient-to-l from-gray-800 py-4 px-3 hover:text-indigo-400">
+                            <BiTrashAlt />
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </aside>
+    )
+}
